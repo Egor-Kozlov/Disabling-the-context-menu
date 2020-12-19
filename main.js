4d1b16469f7c8088857a87f95345542e82c8a765
Egor-Kozlov <60579020+Egor-Kozlov@users.noreply.github.com>
2020-12-19 16:12:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let mainDiv = document.querySelector("#main-div");
+let popupDiv = document.querySelector("#popup-div");
+
+mainDiv.addEventListener("contextmenu", function rightClick(e) {
+  let y = e.clientY + "px";
+  let x = e.clientX + "px";
+  popupDiv.style.visibility = "visible";
+  popupDiv.style.top = y;
+  popupDiv.style.left = x;
+  document.oncontextmenu = function () {
+    return false;
+  };
+});
